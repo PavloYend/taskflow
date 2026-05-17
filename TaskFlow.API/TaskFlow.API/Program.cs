22714bf021d053d32b675eab860d38492de51929
PavloYend <pavlo.yendyk.knm.2020@lpnu.ua>
2026-05-18 02:53:58 +0300

    Fix render port binding

--- TaskFlow.API/TaskFlow.API/Program.cs
+++ TaskFlow.API/TaskFlow.API/Program.cs
@@ -82,6 +82,9 @@ builder.Services.AddCors(options =>
         });
   });
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
