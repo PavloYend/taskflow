4bcc452b3940c74c170275e8ada59163b8780eff
PavloYend <pavlo.yendyk.knm.2020@lpnu.ua>
2026-05-18 03:01:54 +0300

    Enable Swagger in production for deployment testing

--- TaskFlow.API/TaskFlow.API/Program.cs
+++ TaskFlow.API/TaskFlow.API/Program.cs
@@ -96,11 +96,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
